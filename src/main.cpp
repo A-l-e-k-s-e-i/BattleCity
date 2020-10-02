334dfc0918121724eb2429395d7e533fb68e2766
Aleksei <alexshev81@yandex.ru>
2020-10-03 02:18:24 +0500

    OpenGL and glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,86 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+int g_WindowSizeX = 640;
+int g_WindowSizeY = 480;
+        
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) /*Функция изменения окна*/
+{
+    g_WindowSizeX = width;
+    g_WindowSizeY = height;
+    glViewport(0, 0, g_WindowSizeX, g_WindowSizeY); /*Передаём параметры где мы хотим рисовать*/
+}
+/*******************************/
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)  /*Функция обработки клавиш*/
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)  /*Проверяем если нажата ESC и было нажатие*/
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);      /*Указываем glfw что мы хотим закрыть окно. Устанавливаем флаг TRUE */
+    }
+}
+/*******************************/
+
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
     {
+        std::cout << "glfwInit failed!" << std::endl;
+        return -1;
+    }
+
+
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Создаём окно. Create a windowed mode window and its OpenGL context */
+    GLFWwindow* pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "BattleCity", nullptr, nullptr);
+    if (!pWindow)  /*выводим ошибку если окно не создалось*/
+    {
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback); /*Функция активируется при изменении окна*/
+    glfwSetKeyCallback(pWindow, glfwKeyCallback); /*Функция активируется при нажатии клавиш клавиатуры*/
+
+
+       /* Делаем контекст OpenGL для текущего окна Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+	
+	if(!gladLoadGL()) /*Инициализируем glad выводим ошибку если неуспешно*/
+	{
+		std::cout << "Can't load GLAD!" <<std::endl;
+		return -1;
+	}
+
+
+
+        std::cout << "Renderer: " << glGetString(GL_RENDERER) <<std::endl;
+        std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+		
+		glClearColor(1, 1, 0, 1);
+
+
+    /* Главный цикл отрисовки */
+    while (!glfwWindowShouldClose(pWindow)) /*Проверяем состояние окна если закрыто то выходим из цикла*/
     {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        /* Очищаем буфер цвета. Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        /* Меняем местами буфер отрисовки. Swap front and back buffers */
+        glfwSwapBuffers(pWindow);
 
-        /* Poll for and process events */
+        /* Обрабатываем события (мыш, клавиатура и тд.) Poll for and process events */
         glfwPollEvents();
     }
 
-    glfwTerminate();
+    glfwTerminate(); /* Освобождаем ресурсы после выхода из окна*/
     return 0;
 }
